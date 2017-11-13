51adce1cd306296d5a57ca37d115a32f8bb71060
Md. Ahsan Ullah Rasel <raselahmed1612@gmail.com>
2017-11-13 15:27:15 +0600

    Job blog done

---
+++ database/migrations/2017_09_11_162858_create_jobs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('job_title');
+            $table->string('company_name');
+            $table->string('requirements');
+            $table->string('contact');
+            $table->string('skills');
+            $table->string('salary');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
